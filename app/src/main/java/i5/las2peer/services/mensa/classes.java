26c64f1eea5b2776126d3443c0c473aa2d108e40
PhilCAEUser <mail@rwth>
2022-08-29 14:36:21 +0000

    update

--- app/src/main/java/i5/las2peer/services/mensa/classes.java
+++ app/src/main/java/i5/las2peer/services/mensa/classes.java
@@ -9,3 +9,14 @@ public class classes {
 
     
 }
+package i5.las2peer.services.mensa;
+
+import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.ParseException;
+
+public class classes {
+
+
+    
+}
