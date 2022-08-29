7ea1c1c4a328aad87b6caf2e965fcd6041ce6f52
PhilCAEUser <mail@rwth>
2022-08-29 14:39:00 +0000

    update

--- app/src/main/java/i5/las2peer/services/mensa/Mensa.java
+++ app/src/main/java/i5/las2peer/services/mensa/Mensa.java
@@ -377,6 +377,49 @@ public class Mensa extends RESTService {
     return null;
   }
 
+  /**
+   * 
+   * gettest
+   *
+   * 
+   *
+   * 
+   * @return Response 
+   * 
+   */
+  @GET
+  @Path("/test")
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.TEXT_PLAIN)
+  @ApiResponses(value = {
+       @ApiResponse(code = HttpURLConnection.HTTP_OK, message = "res")
+  })
+  @ApiOperation(value = "gettest", notes = " ")
+  public Response gettest() {
+
+
+
+
+     
+    // service method invocations
+
+     
+
+
+
+
+    // res
+    boolean res_condition = true;
+    if(res_condition) {
+      JSONObject res = new JSONObject();
+
+      
+
+      return Response.status(HttpURLConnection.HTTP_OK).entity(res.toJSONString()).build();
+    }
+    return null;
+  }
+
 
 
   }
